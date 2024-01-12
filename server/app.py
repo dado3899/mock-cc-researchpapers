4bc332d5cc2c764640d4f688a0fcfed11d566099
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-01-11 17:34:07 -0700

    Solution

--- server/app.py
+++ server/app.py
@@ -2,9 +2,9 @@
 
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
-from flask_restful import Api
+from flask_restful import Api, Resource
 
-from models import db, Research, Author, ResearchAuthors
+from models import db, Research, Author, ResearchAuthor
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -15,14 +15,63 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
+
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/research')
-def restaurants():
+class Research_Route(Resource):
+    def get(self):
+        all_research = Research.query.all()
+        ar_to_dict = []
+        for research in all_research:
+            ar_to_dict.append(research.to_dict(rules = ('-researchauthors','-authors')))
+        return ar_to_dict,200
+    
+class One_Research_Route(Resource):
+    def get(self, id):
+        research = Research.query.filter(Research.id==id).first()
+        if research:
+            return research.to_dict(),200
+        else:
+            return {"error": "Research paper not found"},400
+    def delete(self,id):
+        research = Research.query.filter(Research.id==id).first()
+        if research:
+            db.session.delete(research)
+            db.session.commit()
+            return {},200
+        else:
+            return {"error": "Research paper not found"},400
 
-    pass
+class Authors_Route(Resource):
+    def get(self):
+        all_author = Author.query.all()
+        aa_to_dict = []
+        for research in all_author:
+            aa_to_dict.append(research.to_dict(rules = ('-researchauthors')))
+        return aa_to_dict,200
+    
+class ResearchAuthor_Route(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            ra = ResearchAuthor(
+                author_id = data["author_id"],
+                research_id = data["research_id"]
+            )
+            db.session.add(ra)
+            db.session.commit()
+            return ra.to_dict(),201
+        except:
+            return {"errors": ["validation errors"]},400
+    
 
+api.add_resource(Research_Route,'/research')
+api.add_resource(One_Research_Route,'/research/<int:id>')
+api.add_resource(Authors_Route,'/author')
+api.add_resource(ResearchAuthor_Route,'/researchauthor')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,3 +11,60 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Research(db.Model,SerializerMixin):
+    __tablename__ = "researches"
+
+    id = db.Column(db.Integer, primary_key = True)
+    topic = db.Column(db.String)
+    year = db.Column(db.Integer)
+    page_count = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    researchauthors = db.relationship("ResearchAuthor", back_populates="research")
+    authors = association_proxy('researchauthors', 'author',
+                                 creator=lambda author_obj: ResearchAuthor(author=author_obj))
+
+    serialize_rules = ('-researchauthors.research',)
+
+    @validates('year')
+    def validate_year(self,key,value):
+        if 1000<=value<=9999:
+            return value
+        else:
+            raise ValueError(value)
+
+
+class Author(db.Model,SerializerMixin):
+    __tablename__ = "authors"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    researchauthors = db.relationship("ResearchAuthor", back_populates="author")
+
+    serialize_rules = ('-researchauthors.author',)
+
+    @validates('field_of_study')
+    def validate_fos(self,key,value):
+        valid_studies = ["AI", "Robotics", "Machine Learning", "Vision", "Cybersecurity"]
+        if value in valid_studies:
+            return value
+        else:
+            raise ValueError("Not valid study")
+
+
+class ResearchAuthor(db.Model,SerializerMixin):
+    __tablename__ = "researchauthors"
+
+    id = db.Column(db.Integer, primary_key = True)
+    author_id = db.Column(db.Integer, db.ForeignKey("authors.id"))
+    research_id = db.Column(db.Integer,  db.ForeignKey("researches.id"))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    author = db.relationship("Author", back_populates="researchauthors")
+    research = db.relationship("Reseach", back_populates="researchauthors")
+
+    serialize_rules = ('-research.researchauthors','-author.researchauthors')
